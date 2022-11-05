ca21165e081489ad6f6e560278d366ac3d01963b
thasipablo <thasipablo@gmail.com>
2022-11-05 23:08:52 +0100

    variables

--- App.js
+++ App.js
@@ -1,1 +1,45 @@
-console.log('Hello world')
+/*
+    * variables
+    ****************/
+
+    // string
+    let name = "John Smith"  
+    const id = 'SFF-9435' 
+ 
+    console.log(name)
+    
+    // numbers
+    let age = 45             
+    let height = 1.65        
+    
+    // boolean 
+    let married = true       
+    let with_child = false  
+ 
+    // objects
+    const address = {
+     city: "Goma",
+     street: "President de la République",
+     country: "DRC"
+    }
+ 
+    // others
+    let person         // undefined
+    const phone = null // null
+ 
+    /*
+     * arithmetic operations */
+ 
+     const a = 12
+     const b = 9
+ 
+     const sum = a + b       // sum
+     const subtract = a - b  // subtraction
+     const multiply = a * b  // multiplication
+     const divide = a / b    // division
+     const modulo = a % b    // modulo
+ 
+     console.log(`${a} + ${b} = ${sum}`)
+     console.log(`${a} - ${b} = ${subtract}`)
+     console.log(`${a} / ${b} = ${divide}`)
+     console.log(`${a} % ${b} = ${modulo}`)
